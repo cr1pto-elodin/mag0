f26ebe1b73a92240cd5e1e2d38b9588d8ffbe857
Victor Polck <victor.polck@redeinspiraeducadores.com.br>
2025-11-12 13:21:20 -0300

    change line reading from file for better error handling

--- src/utils/files.utils.ts
+++ src/utils/files.utils.ts
@@ -1,9 +1,27 @@
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
+import * as readline from 'readline';
 
-export const getFileContentAsync = async (filePath: string): Promise<string> => {
+export const getFileContentAsync = async (filePath: string): Promise<string[]> => {
     try {
-        const content: string = await fs.readFile(filePath,'utf-8')
-        return content;
+        let hasError: boolean = false
+
+        const stream = fs.createReadStream(filePath, 'utf-8')
+        const rl = readline.createInterface({
+            input: stream,
+            crlfDelay: Infinity
+        })
+
+        let lines: string[] = [];
+
+        for await (const line of rl) {
+            lines.push(line)
+        }
+
+        if (hasError) {
+            throw new Error('The file has mag0 content error')
+        }
+
+        return lines;
     } catch (error: any) {
         console.log(`Error while reading file: ${error.message}`)
         throw error;
