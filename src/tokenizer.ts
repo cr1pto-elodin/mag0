986a555a2534e494b2cc83c57b6b3dcd42ff7a26
Victor Polck <victor.polck@redeinspiraeducadores.com.br>
2025-11-12 16:50:35 -0300

    logic to get numbers in tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,4 +1,5 @@
 import { keyWords } from "./keywords/mapper/keyword.mapper"
+import { TokenTypeEnum } from "./tokens/enum/tokens.enum"
 import { Token } from "./tokens/model/token.model"
 
 const delimiters = /[A-Za-z_]+|\d+|[^\s]/g
@@ -12,16 +13,30 @@ export const tokenizeSingleLine = (content: string, line: number) => {
 
     const tokens: Token[] = []
 
-    for (const [index,contentToken] of contentTokens.entries()) {
+    for (const [index, contentToken] of contentTokens.entries()) {
         if (contentToken in keyWords) {
             const token = new Token({
                 type: keyWords[contentToken],
                 value: contentToken,
                 line: line,
-                column: index+1
+                column: index + 1
             })
 
             tokens.push(token)
+            continue
+        }
+
+        if (!isNaN(Number(contentToken))) {
+            const token = new Token({
+                type: TokenTypeEnum.NUMBER,
+                value: contentToken,
+                line: line,
+                column: index
+            })
+
+            tokens.push(token)
+
+            continue
         }
     }
 
@@ -31,8 +46,8 @@ export const tokenizeSingleLine = (content: string, line: number) => {
 export const tokenizeFileContent = (lines: string[]) => {
     const tokenPerLines: Token[][] = []
 
-    for (const [i,line] of lines.entries()) {
-        const tokensInLine = tokenizeSingleLine(line,i+1)
+    for (const [i, line] of lines.entries()) {
+        const tokensInLine = tokenizeSingleLine(line, i + 1)
         tokenPerLines.push(tokensInLine)
     }
 
