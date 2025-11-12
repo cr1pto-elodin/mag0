f5e1e8c4dd99670b07f94cbfb8e7e933620b0f65
Victor Polck <victor.polck@redeinspiraeducadores.com.br>
2025-11-12 15:16:52 -0300

    adding all tokens to enum and its mapper

--- src/keywords/mapper/keyword.mapper.ts
+++ src/keywords/mapper/keyword.mapper.ts
@@ -1,7 +1,43 @@
 import { TokenTypeEnum } from "../../tokens/enum/tokens.enum";
 
-const keyWords: Record<string, TokenTypeEnum> = {
-    'print': TokenTypeEnum.PRINT,
-    '+': TokenTypeEnum.ADD,
+export const keyWords: Record<string, TokenTypeEnum> = {
+    // Single-character tokens
+    '(': TokenTypeEnum.LEFT_PARENTHESES,
+    ')': TokenTypeEnum.RIGHT_PARENTHESES,
+    '{': TokenTypeEnum.LEFT_BRACE,
+    '}': TokenTypeEnum.RIGHT_BRACE,
+    ',': TokenTypeEnum.COMMA,
+    '.': TokenTypeEnum.DOT,
+    '-': TokenTypeEnum.MINUS,
+    '+': TokenTypeEnum.PLUS,
+    ';': TokenTypeEnum.SEMICOLON,
+    '/': TokenTypeEnum.SLASH,
+    '*': TokenTypeEnum.STAR,
+    '"': TokenTypeEnum.QUOTATION_MARKS,
+
+    // One or two-character tokens
+    '!': TokenTypeEnum.BANG,
+    '!=': TokenTypeEnum.BANG_EQUAL,
+    '=': TokenTypeEnum.EQUAL,
+    '==': TokenTypeEnum.EQUAL_EQUAL,
+    '>': TokenTypeEnum.GREATER,
+    '>=': TokenTypeEnum.GREATER_EQUAL,
+    '<': TokenTypeEnum.LESS,
+    '<=': TokenTypeEnum.LESS_EQUAL,
+
+    // Keywords
+    'and': TokenTypeEnum.AND,
+    'or': TokenTypeEnum.OR,
     'if': TokenTypeEnum.IF,
-}
+    'else': TokenTypeEnum.ELSE,
+    'true': TokenTypeEnum.TRUE,
+    'false': TokenTypeEnum.FALSE,
+    'for': TokenTypeEnum.FOR,
+    'while': TokenTypeEnum.WHILE,
+    'print': TokenTypeEnum.PRINT,
+    'return': TokenTypeEnum.RETURN,
+    'class': TokenTypeEnum.CLASS,
+    'this': TokenTypeEnum.THIS,
+    'var': TokenTypeEnum.VAR,
+    'nil': TokenTypeEnum.NIL,
+};
--- src/tokens/enum/tokens.enum.ts
+++ src/tokens/enum/tokens.enum.ts
@@ -1,19 +1,46 @@
 export enum TokenTypeEnum {
     // Literals
     NUMBER = 'NUMBER',
+    STRING = 'STRING',
     IDENTIFIER = 'IDENTIFIER',
-
-    //Keywords
-    PRINT = 'PRINT',
-    IF = 'IF',
-    FOR = 'FOR',
-    
-    //Operators
-    ADD = 'ADD',
-    ASSIGN = 'ASSIGN',
-
-    //Separators
+        
+    //Single token characters
     LEFT_PARENTHESES = 'LEFT_PARENTHESES',
     RIGHT_PARENTHESES = 'RIGHT_PARENTHESES',
-    QUOTATION_MARKS = 'QUOTATION_MARKS'
+    QUOTATION_MARKS = 'QUOTATION_MARKS',
+    RIGHT_BRACE = 'RIGHT_BRACE',
+    LEFT_BRACE = 'LEFT_BRACE',
+    COMMA = 'COMMA',
+    DOT = 'DOT',
+    MINUS = 'MINUS',
+    PLUS = 'PLUS',
+    SEMICOLON = 'SEMICOLON',
+    SLASH = 'SLASH',
+    STAR = 'STAR',
+
+    //One or two characters token
+    BANG = 'BANG',
+    BANG_EQUAL = 'BANG_EQUAL',
+    EQUAL = 'EQUAL',
+    EQUAL_EQUAL = 'EQUAL_EQUAL',
+    GREATER = 'GREATER',
+    GREATER_EQUAL = 'GREATER_EQUAL',
+    LESS = 'LESS',
+    LESS_EQUAL = 'LESS_EQUAL',
+
+    //Keywords
+    AND = 'AND',
+    OR = 'OR',
+    IF = 'IF',
+    ELSE = 'ELSE',
+    TRUE = 'TRUE',
+    FALSE = 'FALSE',
+    FOR = 'FOR',
+    WHILE = 'WHILE',
+    PRINT = 'PRINT',
+    RETURN = 'RETURN',
+    CLASS = 'CLASS',
+    THIS = 'THIS',
+    VAR = 'VAR',
+    NIL = 'NIL',
 }
