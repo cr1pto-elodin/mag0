51b37329e0b3bf08163a522035fd5effffe33ca9
Victor Polck <victor.polck@redeinspiraeducadores.com.br>
2025-11-13 11:33:26 -0300

    change tokenizer file to Tokenizer Class

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -2,117 +2,142 @@ import { keyWords } from "./keywords/mapper/keyword.mapper"
 import { TokenTypeEnum } from "./tokens/enum/tokens.enum"
 import { Token } from "./tokens/model/token.model"
 
-const delimiters = /[A-Za-z_]+|\d+|[^\s]/g
-const identifierRegEx = /^[A-Za-z_]+$/
-const numberRegex = /^\d+$/
+export class Tokenizer {
+    private delimiters = /[A-Za-z_]+|\d+|[^\s]/g
+    private identifierRegEx = /^[A-Za-z_]+$/
+    private numberRegex = /^\d+$/
 
-export const tokenizeSingleLine = (content: string, line: number) => {
-    const contentTokens = content.match(delimiters)
+    private readStringToken(
+        contentToken: Token,
+        line: number,
+        column: number
+    ) {
 
-    if (contentTokens === null) {
-        return []
     }
 
-    const tokens: Token[] = []
-    let insideString = false;
-    let stringBuffer = '';
-    let stringStartColumn = 0;
+    private readNumberToken(
+        contentToken: string,
+        line: number,
+        column: number
+    ) {
+        return new Token({
+            type: TokenTypeEnum.NUMBER,
+            value: contentToken,
+            line: line,
+            column: column
+        })
+    }
 
-    for (let index = 0; index < contentTokens.length; index++) {
-        const contentToken = contentTokens[index]!
+    public tokenizeSingleLine(
+        content: string,
+        line: number
+    ) {
+        const contentTokens = content.match(this.delimiters)
 
-        //Strings
-        if (contentToken === '"') {
+        if (contentTokens === null) {
+            return []
+        }
+
+        const tokens: Token[] = []
+        let insideString = false;
+        let stringBuffer = '';
+        let stringStartColumn = 0;
+
+        for (let index = 0; index < contentTokens.length; index++) {
+            const contentToken = contentTokens[index]!
+
+            //Strings
+            if (contentToken === '"') {
+                if (insideString) {
+                    tokens.push(
+                        new Token({
+                            type: TokenTypeEnum.STRING,
+                            value: stringBuffer,
+                            line,
+                            column: stringStartColumn,
+                        })
+                    )
+
+                    stringBuffer = ''
+                } else {
+                    stringStartColumn = index + 1
+                }
+                insideString = !insideString
+                continue
+            }
+
             if (insideString) {
+                stringBuffer += contentToken
+                continue
+            }
+
+            //Multi char operators
+            const nextToken = contentTokens[index + 1]
+            let combined = contentToken + (nextToken ?? '')
+            if (combined in keyWords) {
                 tokens.push(
                     new Token({
-                    type: TokenTypeEnum.STRING,
-                    value: stringBuffer,
-                    line,
-                    column: stringStartColumn,
+                        type: keyWords[combined],
+                        value: combined,
+                        line,
+                        column: index + 1,
                     })
                 )
-
-                stringBuffer = ''
-            } else {
-                stringStartColumn = index + 1
+                index++
+                continue
             }
-            insideString = !insideString
-            continue
-        }
 
-        if (insideString) {
-            stringBuffer += contentToken
-            continue
-        }
-
-        //Multi char operators
-        const nextToken = contentTokens[index + 1]
-        let combined = contentToken + (nextToken ?? '')
-        if (combined in keyWords) {
-            tokens.push(
-                new Token({
-                    type: keyWords[combined],
-                    value: combined,
-                    line,
-                    column: index + 1,
-                })
-            )
-            index++
-            continue
-        }
-
-        //Single char operators and keywords
-        if (contentToken in keyWords) {
-            const token = new Token({
-                type: keyWords[contentToken],
-                value: contentToken,
-                line: line,
-                column: index + 1
-            })
-
-            tokens.push(token)
-            continue
-        }
-
-        //Numbers
-        if (numberRegex.test(contentToken)) {
-            const token = new Token({
-                type: TokenTypeEnum.NUMBER,
-                value: contentToken,
-                line: line,
-                column: index
-            })
-
-            tokens.push(token)
-
-            continue
-        }
-
-        //Identifiers
-        if (identifierRegEx.test(contentToken)) {
-            tokens.push(
-                new Token({
-                    type: TokenTypeEnum.IDENTIFIER,
+            //Single char operators and keywords
+            if (contentToken in keyWords) {
+                const token = new Token({
+                    type: keyWords[contentToken],
                     value: contentToken,
-                    line,
-                    column: index + 1,
+                    line: line,
+                    column: index + 1
                 })
-            );
-          continue;
+
+                tokens.push(token)
+                continue
+            }
+
+            //Numbers
+            if (this.numberRegex.test(contentToken)) {
+                const token = this.readNumberToken(
+                    contentToken,
+                    line,
+                    index + 1
+                )
+
+                tokens.push(token)
+
+                continue
+            }
+
+            //Identifiers
+            if (this.identifierRegEx.test(contentToken)) {
+                tokens.push(
+                    new Token({
+                        type: TokenTypeEnum.IDENTIFIER,
+                        value: contentToken,
+                        line,
+                        column: index + 1,
+                    })
+                );
+                continue;
+            }
         }
+
+        return tokens
     }
 
-    return tokens
-}
+    public tokenizeFileContent(lines: string[]) {
+        const tokenPerLines: Token[][] = []
 
-export const tokenizeFileContent = (lines: string[]) => {
-    const tokenPerLines: Token[][] = []
+        for (const [i, line] of lines.entries()) {
+            const tokensInLine = this.tokenizeSingleLine(line, i + 1)
+            tokenPerLines.push(tokensInLine)
+        }
 
-    for (const [i, line] of lines.entries()) {
-        const tokensInLine = tokenizeSingleLine(line, i + 1)
-        tokenPerLines.push(tokensInLine)
+        return tokenPerLines
     }
-
-    return tokenPerLines
 }
